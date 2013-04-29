95510d364e78714beb4b5c2d8caabd18ef27cecf
unknown <allender@ALLENDER-THINK.(none)>
2013-04-29 09:53:36 -0500

    New update bundle file to work on windows

--- vim/update_bundles.py
+++ vim/update_bundles.py
@@ -4,12 +4,14 @@
 #
 
 import os
+import stat
 import shutil
 import subprocess
 
 git_bundles = [
         'git://github.com/tpope/vim-fugitive.git',
         'git://github.com/altercation/vim-colors-solarized.git',
+	'git://repo.or.cz/vcscommand',
 ];
 
 # clear out folders in the bundle folder so that we can restore
@@ -17,22 +19,30 @@ git_bundles = [
 
 os.chdir( 'bundle' )
 
+def remove_readonly(fn, path, excinfo):
+    if fn is os.rmdir:
+        os.chmod(path, stat.S_IWRITE)
+        os.rmdir(path)
+    elif fn is os.remove:
+        os.chmod(path, stat.S_IWRITE)
+        os.remove(path)
+
 for root,dirs,files in os.walk('.'):
     for f in files:
         os.unlink( os.path.join(root, f) )
     for d in dirs:
-        shutil.rmtree( os.path.join(root, d) )
+        shutil.rmtree( os.path.join(root, d), onerror=remove_readonly )
 
 # for each bundle, pull from repository
 
 for bundle in git_bundles:
     bundle_name = bundle.rsplit('/', 1)[1].replace('.git','')
     print 'Cloning {0}'.format( bundle )
-    subprocess.call( ['git', 'clone', bundle] )
+    subprocess.call( ['git', 'clone', bundle], shell=True )
 
     # remove the .git folder in the bundle so that we can actually commit the
     # folders to git without having them look like sub repositories
-    shutil.rmtree( os.path.join(bundle_name, '.git') )
+    shutil.rmtree( os.path.join(bundle_name, '.git'), onerror=remove_readonly )
 
 os.chdir( '..' )
 
