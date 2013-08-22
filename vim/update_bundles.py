0780a79edde142b9e6c8a6f4a9bc4c49f151966c
Mark Allender <mark.allender@gmail.com>
2013-08-22 12:52:12 +0800

    Added nerdcommenter

--- vim/update_bundles.py
+++ vim/update_bundles.py
@@ -9,9 +9,10 @@ import shutil
 import subprocess
 
 git_bundles = [
-        'git://github.com/tpope/vim-fugitive.git',
-        'git://github.com/altercation/vim-colors-solarized.git',
+    'git://github.com/tpope/vim-fugitive.git',
+    'git://github.com/altercation/vim-colors-solarized.git',
 	'git://repo.or.cz/vcscommand',
+    'https://github.com/scrooloose/nerdcommenter.git',
 ];
 
 # clear out folders in the bundle folder so that we can restore
