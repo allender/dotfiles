ea3659759ab3de73b87891fbf8d0f26870eef9ec
Mark Allender <mark.allender@gmail.com>
2016-07-08 15:24:44 -0500

    remove vim-love-docs.  Don't want it changing my color scheme

--- vim/bundle/vim-love-docs/src/syntax/gen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd "$(dirname "$0")"
-set -e
-rm -rf love-api
-git clone https://github.com/love2d-community/love-api
-mkdir -p ../../after/syntax/
-love lua > ../../after/syntax/lua.vim
-love love-conf > ../../after/syntax/love-conf.vim
-rm -rf love-api
